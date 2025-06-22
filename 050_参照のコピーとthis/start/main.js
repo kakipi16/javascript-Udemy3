f951e85f936cfbae8670e2bebd1cf07d0bd2c60c
kakipi16 <kakipi533@gmail.com>
2025-06-22 23:12:01 +0900

    050学習完了

--- 050_参照のコピーとthis/start/main.js
+++ 050_参照のコピーとthis/start/main.js
@@ -1,7 +1,29 @@
+window.name = 'John'
+
 const person = {
     name: 'Tom',
     hello: function() {
         console.log('Hello ' + this.name);
+        a();
+
+        const person = {
+            name: 'Tim',
+            hello: function() {
+                console.log('Hello ' + this.name);
+                a();
+            }
+        }
+        person.hello();
     }
 }
+// const ref = person.hello;
+// ref();
+
+//personオブジェクトを参照するのでhello Tomと出力される
 person.hello();
+
+function a() {
+    //グローバルオブジェクトを参照するのでhello Johnと出力される
+    console.log('Hello ' + this.name)
+}
+
