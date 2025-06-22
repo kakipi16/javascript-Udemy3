877804c718ec259a9d47df98fd5180a6fc3a77a3
kakipi16 <kakipi533@gmail.com>
2025-06-22 23:00:17 +0900

    040学習完了

--- 040_this/start/main.js
+++ 040_this/start/main.js
@@ -1,6 +1,11 @@
 const person = {
     name: 'Tom',
     hello: function() {
+        console.log("Hello" + this.name)
 
+        //これでも出力されるが、メソッドの中では記載しない。
+        // console.log("Hello" + person.name)
     }
 }
+//
+person.hello();
