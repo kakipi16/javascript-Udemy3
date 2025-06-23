5d7bb210bd4a298e7a3c2291b334c2a7c760f9e3
kakipi16 <kakipi533@gmail.com>
2025-06-23 16:37:36 +0900

    070学習完了

--- 070_bindとthis/start/main.js
+++ 070_bindとthis/start/main.js
@@ -7,9 +7,27 @@ const person = {
     }
 }
 person.hello();
+//bind(person)のpersonはオブジェクト
+const helloTom = person.hello.bind(person);
 
 function fn(ref) {
     ref();
 }
+//Hello Tomと出力される
+fn(helloTom);
 
-fn(person.hello);
+function a() {
+    console.log("hello " + this.name)
+}
+
+const b = a.bind({name: "Tim"});
+//hello Timと出力される
+b();
+
+function c(name) {
+    console.log("hello " + this.name)
+}
+//bindは引数を取った値を束縛し、固定する。。
+const d = a.bind(null, "Tim");
+//hello Timと出力される。
+b("Tom");
