0b3eaba859a596b32c4de7241cf3485241ff89ee
kakipi16 <kakipi533@gmail.com>
2025-06-27 17:08:03 +0900

    100学習完了

--- 100_アロー関数とthis/start/main.js
+++ 100_アロー関数とthis/start/main.js
@@ -1,9 +1,33 @@
 window.name = 'John';
 
+
 const person = {
     name: 'Tom',
-    hello: function() {
+    //このように省略できる。
+    //もともとはhello: function()
+    hello() {
         console.log('Hello ' + this.name);
+        //この場合の出力結果はBye Tom
+        const a = () => console.log('Bye ' + this.name);
+        a();
     }
 }
-person.hello();
+//hello Tomが出力される。
+// person.hello();
+
+function b() {
+    const a = () => console.log('Bye ' + this.name);
+    a();
+}
+//Bey Johnが出力される。
+b()
+
+// const person1 = {
+//     name: 'Tom',
+//     hello: () => {
+//         console.log('Hello ' + this.name);
+//     }
+// }
+// //hello Johnが出力される。
+// person1.hello();
+
