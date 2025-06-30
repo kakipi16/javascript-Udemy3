6153306251cfaee60e3402c8e2993cd00768fc40
kakipi16 <kakipi533@gmail.com>
2025-06-30 16:32:15 +0900

    940 OK完璧

--- 930_修了確認_クロージャーとコールバック/start/main.js
+++ 930_修了確認_クロージャーとコールバック/start/main.js
@@ -3,16 +3,20 @@
  * コールバックとクロージャーの仕組みを使って、
  * setTimeoutで1秒後に"hello Tom"と表示されるような
  * hello関数を作成してみましょう。
- * 
+ *
  * 条件としてはhello関数の引数には必ず
  * nameの引数を取るものとします。
- * 
+ *
  */
+//
 function hello(name) {
-  
+    return function() {
+        console.log("hello " + name);
+    }
 }
 
 /**
  * 実行文は以下の通りです。
  */
+//このsetTimeoutは実行されてしまっているので、hello関数は戻り値に関数をつけてあげる。
 setTimeout(hello('Tom'), 1000);
