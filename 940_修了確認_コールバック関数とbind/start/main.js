14aeb387957b327b079be63db44d0fbc8fb05bb7
kakipi16 <kakipi533@gmail.com>
2025-07-03 16:19:30 +0900

    940おしい 問題文よく読んで

--- 940_修了確認_コールバック関数とbind/start/main.js
+++ 940_修了確認_コールバック関数とbind/start/main.js
@@ -5,32 +5,41 @@
  * ようにcalcFactory関数内の実装を変更してみてください。
  */
 function calcFactory(val, callback) {
+    //答え
+    function callbackAfter1s(str) {
+        setTimeout(callback.bind(null, str), 1000);
+    }
+
     return {
         plus: function(target) {
             const newVal = val + target;
-            callback(`${val} + ${target} = ${newVal}`);
+            callbackAfter1s(`${val} + ${target} = ${newVal}`);
             val = newVal;
         },
         minus: function(target) {
             const newVal = val - target;
-            callback(`${val} - ${target} = ${newVal}`);
+            callbackAfter1s(`${val} - ${target} = ${newVal}`);
             val = newVal;
         },
         multiply: function(target) {
             const newVal = val * target;
-            callback(`${val} x ${target} = ${newVal}`);
+            callbackAfter1s(`${val} x ${target} = ${newVal}`);
             val = newVal;
         },
         divide: function(target) {
             const newVal = val / target;
-            callback(`${val} / ${target} = ${newVal}`);
+            callbackAfter1s(`${val} / ${target} = ${newVal}`);
             val = newVal;
         }
     };
 }
 
-const calc = calcFactory(10, console.log);
-calc.plus(5); 
-calc.minus(3); 
+//解答
+// const calc = calcFactory(10, alert);
+// setTimeout(calc.plus.bind(null, 5), 1000);
+
+const calc = calcFactory(10, alert);
+calc.plus(5);
+calc.minus(3);
 calc.multiply(3);
 calc.divide(2);
